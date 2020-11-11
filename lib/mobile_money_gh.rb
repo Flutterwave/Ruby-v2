da0009c5456cd94c1a6db4e5ea8baee99ee930ad
Cornelius Ashley-Osuzoka <59456456+Corvus97@users.noreply.github.com>
2020-11-11 14:23:05 +0100

    Update mobile_money_gh.rb

--- lib/mobile_money_gh.rb
+++ lib/mobile_money_gh.rb
@@ -2,7 +2,7 @@ require_relative 'rave_ruby'
 
 
 # This is a rave object which is expecting public and secret keys
-rave = RaveRuby.new("FLWPUBK-db73db4554b5116420d2864fd7887afa-X", "FLWSECK-6af1c304477b39b47fa55ae94fa3b29d-X", true)
+rave = RaveRuby.new("FLWPUBK-****************************-X", "FLWSECK-****************************-X", true)
 
 
 # This is used to perform GHS charge
